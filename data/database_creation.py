de9dab43ace1ffc44dc159c9b9def1d6e3cc741e
ArmaanG06 <agandhara06@gmail.com>
2026-01-11 16:00:33 -0500

    Version 0.0.2. Changed structure

---
+++ data/database_creation.py
@@ -0,0 +1,5 @@
+import duckdb
+
+# 1️⃣ Connect to (or create) a DuckDB database file
+conn = duckdb.connect(r'C:\Users\agand\Desktop\Personal_Coding\Finance_Projects\Midday_correction_strategy\data\market.duckdb')
+
