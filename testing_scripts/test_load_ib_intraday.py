3516481837862ffe90f366940bd1e9f74a7ff647
zmhasham <hash9154@mylaurier.ca>
2026-03-12 13:13:10 -0400

    requirements.txt

--- testing_scripts/test_load_ib_intraday.py
+++ testing_scripts/test_load_ib_intraday.py
@@ -6,9 +6,9 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 from src.mrs.data.main_load import load_ib_intraday_data
 
 def main():
-    symbols = [str(input("Enter your ticker: "))]
-    duration =str(input("Enter your duration (IB Compatble): "))
-    bar_size = str(input("Enter your bar-size (IB Compatable): "))
+    symbols = [str(input("Enter Ticker Symbol (ex. AAPL, LMT, MSFT)): "))]
+    duration = str(input("Enter Duration (IB Compatble): "))
+    bar_size = str(input("Enter Intraday Bar-Size (IB Compatable): "))
     df = load_ib_intraday_data(symbols=symbols, duration=duration, bar_size=bar_size)
     print(df.head())
 
