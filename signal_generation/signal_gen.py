1f8bff90fca8520822f50ac2e5f78dcb67841533
ArmaanG06 <agandhara06@gmail.com>
2026-03-12 10:17:25 -0400

    Version 0.0.2. added some signal gen code but not complete

--- signal_generation/signal_gen.py
+++ signal_generation/signal_gen.py
@@ -1,35 +1,66 @@
 from dataclasses import dataclass
-#import loader from #Zakirs data puller
+#from Zakirs data puller import loader once he is done
 import pandas as pd
 
-@dataclass
-class SignalParams:
-    rebound_threshold: float
-    vwap_deviation_threshold: float
-    volume_z_threshold: float
-    start_time: str
-    end_time: str
-    max_hold_bars: int
+COLUMN_NAMES = ["ticker", "timestamp", "entry", "position", "price", "amount"]
 
-def generate_signals_from_df(bars: pd.DataFrame, params: SignalParams,) -> pd.DataFrame:
-    # pure function: assumes bars already filtered
+params_1m = {
+    "bar_size": 1,
+    "price_inc" : 0.05,
+    "vol_inc" : 0.05,
+    "entry_time_1" : 1, #hours into session
+    "exit_time_1" : 3
+}
+
+params_5m = {
+    "bar_size": 5,
+    "price_inc" : 0.05,
+    "vol_inc" : 0.05,
+    "entry_time_1" : 1.5, #hours into session
+    "exit_time_1" : 3
+}
+
+params_15m = {
+    "bar_size": 15,
+    "price_inc" : 0.05,
+    "vol_inc" : 0.05,
+    "entry_time_1": 0.5, #hours into session
+    "exit_time_1": 4,
+    "entry_time_2": 3,
+    "exit_time_2": 6
+}
+
+
+
+def generate_signals_from_df(bars: pd.DataFrame, params, param_version:"1"):
+    
+    signals_df = pd.DataFrame(columns=COLUMN_NAMES)
+
+    bar_size = params["bar_size"]
+    bar_delta_entry = params[f"entry_time_{param_version}"] * (60/bar_size)
+    bar_delta_exit = params[f"exit_time_{param_version}"] * (60/bar_size)
+    
+    price_change = (bars[bar_delta_entry]["open"] - bars[0]["open"])/bars[0]["open"]
+    vol_change = (bars[bar_delta_entry]["volume"] - bars[0]["volume"])/bars[0]["volume"]
+
+    if price_change > params["price_inc"] and vol_change > params["price_inc"]:
+        signals_df = []
+
+
     pass
 
-class BarsLoader:
-    def load(self, symbol: str, start: str, end: str, bar_size: str, columns: list[str] | None = None) -> pd.DataFrame:
-        #SQL COMMAND TO INTERACT WITH MARKET-DATA SUPABASE DB TO GRAB DATA IN THE FORM OF A PANDAS DF
-        pass
+
+def clean_signals(signals_raw):
+    pass
 
 
-def generate_signals(symbol: str, start: str, end: str, bar_size: str, params: SignalParams) -> pd.DataFrame:
-    bars = loader.load(symbol, start, end, bar_size,columns=["symbol", "time_stamp_tz", "bar_size", "o", "h", "l", "c", "v", "vwap"])
+def generate_signals(symbol: str, start: str, end: str, bar_size: str, params) -> pd.DataFrame:
+    bars = loader.load(symbol, start, end, bar_size)
+
     #Trial params (should actually me in the main file where func is called but is here for now):
-    params = SignalParams(
-    rebound_threshold=0.02,
-    vwap_deviation_threshold=-0.015,
-    volume_z_threshold=2.0,
-    start_time="11:00",
-    end_time="13:30",
-    max_hold_bars=20
-)
-    return generate_signals_from_df(bars, params)
+    
+    signals_raw = generate_signals_from_df(bars, params, "1") #will return a df that contains more information
+    signals_pure = clean_signals(signals_raw) #will return a df that only contains the entry (1,0,-1) and position (1,0,-1)
+    return signals_raw, signals_pure
+
+
